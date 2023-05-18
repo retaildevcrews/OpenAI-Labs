e79ac64af4c864e9b321ea126c6cf483104a90bf
Jorge <lunajorge@microsoft.com>
2023-05-18 23:37:19 +0000

    adding post create script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -19,6 +19,3 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
-nbstripout --install
-nbstripout --install --attributes .gitattributes
-nbstripout --install --global
