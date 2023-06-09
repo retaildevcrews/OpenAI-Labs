91f1c1e69c516f2811f1f16edd0b6cd8cf80fd1d
Jorge Luna <lunajorge@microsoft.com>
2023-06-09 10:11:19 -0500

    updating start

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,3 @@
-## add local bin directory to path
-
 ## update and install some things we should probably have
 ## setup and install oh-my-zsh
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -8,7 +6,7 @@ sudo cp /root/.zshrc /home/$USERNAME
 sudo sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 sudo chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
-
+## set up nbscript
 nbstripout --install
 nbstripout --install --attributes .gitattributes
 nbstripout --install --global
