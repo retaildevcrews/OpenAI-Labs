86fb6823309d26e77b239613f2749d04674073b5
Jorge <lunajorge@microsoft.com>
2023-06-09 16:17:31 +0000

    adding ohmyzsh to container

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,13 @@
 ## update and install some things we should probably have
 echo "post create started" >> $HOME/status
 
-## set up nbscript
+## set up nbscript in order to clean jupyter notebook files on check in to git
 nbstripout --install
 nbstripout --install --attributes .gitattributes
 nbstripout --install --global
+## set git default reconciliation scheme
+git config pull.rebase false
+## install ohmyzsh
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 
 echo "post create completed" >> $HOME/status
