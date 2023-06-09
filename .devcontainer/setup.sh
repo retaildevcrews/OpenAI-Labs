a672fe2b3782a1be1956927c7a828dcc6a5ecc53
Jorge <lunajorge@microsoft.com>
2023-06-09 15:41:08 +0000

    testing out this thing

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,10 +1,5 @@
 ## update and install some things we should probably have
-## setup and install oh-my-zsh
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo cp -R /root/.oh-my-zsh /home/$USERNAME
-sudo cp /root/.zshrc /home/$USERNAME
-sudo sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-sudo chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
 
 ## set up nbscript
 nbstripout --install
