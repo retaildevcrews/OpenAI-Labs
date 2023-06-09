c85de02c77a7aa1cc8eda0d072f569bb5a632a5f
Jorge <lunajorge@microsoft.com>
2023-06-09 15:48:05 +0000

    test

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,9 @@
 ## update and install some things we should probably have
-
+echo "post create started" >> $HOME/status
 
 ## set up nbscript
 nbstripout --install
 nbstripout --install --attributes .gitattributes
 nbstripout --install --global
+
+echo "post create completed" >> $HOME/status
